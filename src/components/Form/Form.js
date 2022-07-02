2fcb4ea2ca659ce7aab27fba8a11e1807996b5e5
Sneha Shaw <snehashaw122@gmail.com>
2022-07-02 20:55:33 +0530

    second commit

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -84,7 +84,7 @@ const Form = ({ currentId, setCurrentId }) => {
         </div>
         <div className={classes.fileInput}><FileBase type="file" multiple={false} onDone={({ base64 }) => setPostData({ ...postData, selectedFile: base64 })} /></div>
         <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>Submit</Button>
-        <Button variant="contained" color="secondary" size="small" onClick={clear} fullWidth>Clear Form</Button>
+        <Button variant="contained" color="secondary" size="small" onClick={clear} fullWidth>Clear</Button>
       </form>
     </Paper>
   );
